edd0b17d99b9ce10809b5de249fb0d802c314494
Zion Pu <pu.007@qq.com>
2025-01-24 13:52:54 +0800

    feat(nvim.user/.config/nvim/lua/plugins/general.lua): add file selector provider and update plugins configuration

--- nvim.user/.config/nvim/lua/plugins/general.lua
+++ nvim.user/.config/nvim/lua/plugins/general.lua
@@ -22,6 +22,12 @@ return {
           model = "qwen2.5-coder",
         },
       },
+      file_selector = {
+        --- @alias FileSelectorProvider "native" | "fzf" | "mini.pick" | "snacks" | "telescope" | string
+        provider = "fzf",
+        -- Options override for custom providers
+        provider_opts = {},
+      },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
@@ -71,7 +77,6 @@ return {
           "avante_mentions",
           "avante_files",
         },
-        cmdline = {},
         providers = {
           avante_commands = {
             name = "avante_commands",
@@ -92,10 +97,6 @@ return {
             opts = {},
           },
         },
-      },
-      keymap = {
-        preset = "enter",
-        ["<C-y>"] = { "select_and_accept" },
       },
     },
   },
