869506a35ded3ca6ed5e2e523313f6366ed467d3
Zion Pu <pu.007@qq.com>
2025-02-23 00:53:15 +0800

    chore: update AI plugin to include Copilot and Copilot-Chat, remove deprecated blink.cmp

--- nvim.user/.config/nvim/lua/plugins/general.lua
+++ nvim.user/.config/nvim/lua/plugins/general.lua
@@ -74,44 +74,44 @@ return {
       },
     },
   },
-  {
-    "saghen/blink.cmp",
-    opts = {
-      sources = {
-        compat = { "avante_commands", "avante_mentions", "avante_files" },
-        default = {
-          "lsp",
-          "cmdline",
-          "path",
-          "snippets",
-          "buffer",
-          "avante_commands",
-          "avante_mentions",
-          "avante_files",
-        },
-        providers = {
-          avante_commands = {
-            name = "avante_commands",
-            module = "blink.compat.source",
-            score_offset = 90, -- show at a higher priority than lsp
-            opts = {},
-          },
-          avante_files = {
-            name = "avante_files",
-            module = "blink.compat.source",
-            score_offset = 100, -- show at a higher priority than lsp
-            opts = {},
-          },
-          avante_mentions = {
-            name = "avante_mentions",
-            module = "blink.compat.source",
-            score_offset = 1000, -- show at a higher priority than lsp
-            opts = {},
-          },
-        },
-      },
-    },
-  },
+  -- {
+  --   "saghen/blink.cmp",
+  --   opts = {
+  --     sources = {
+  --       compat = { "avante_commands", "avante_mentions", "avante_files" },
+  --       default = {
+  --         "lsp",
+  --         "cmdline",
+  --         "path",
+  --         "snippets",
+  --         "buffer",
+  --         "avante_commands",
+  --         "avante_mentions",
+  --         "avante_files",
+  --       },
+  --       providers = {
+  --         avante_commands = {
+  --           name = "avante_commands",
+  --           module = "blink.compat.source",
+  --           score_offset = 90, -- show at a higher priority than lsp
+  --           opts = {},
+  --         },
+  --         avante_files = {
+  --           name = "avante_files",
+  --           module = "blink.compat.source",
+  --           score_offset = 100, -- show at a higher priority than lsp
+  --           opts = {},
+  --         },
+  --         avante_mentions = {
+  --           name = "avante_mentions",
+  --           module = "blink.compat.source",
+  --           score_offset = 1000, -- show at a higher priority than lsp
+  --           opts = {},
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
