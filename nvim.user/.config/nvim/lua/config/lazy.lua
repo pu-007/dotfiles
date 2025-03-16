d4964826f1e75415d9189efe4144f8890f916ae4
Zion Pu <pu.007@qq.com>
2025-03-16 14:58:22 +0800

    fix: enable plugin update checker and uncomment Python settings

--- nvim.user/.config/nvim/lua/config/lazy.lua
+++ nvim.user/.config/nvim/lua/config/lazy.lua
@@ -25,19 +25,19 @@ require("lazy").setup({
     version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = {},
-  checker = { enabled = false }, -- automatically check for plugin updates
+  checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
       disabled_plugins = {
-        "gzip",
-        "matchit",
-        "matchparen",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
+        -- "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        -- "tarPlugin",
+        -- "tohtml",
+        -- "tutor",
+        -- "zipPlugin",
       },
     },
   },
--- nvim.user/.config/nvim/lua/config/options.lua
+++ nvim.user/.config/nvim/lua/config/options.lua
@@ -3,15 +3,15 @@
 local opt = vim.opt
 
 opt.scrolloff = 8 -- Lines of context
-opt.wrap = false -- Disable line wrap
+opt.wrap = true
 opt.colorcolumn = { 80 }
 
-vim.g.lazyvim_python_lsp = "pyright"
-vim.g.lazyvim_python_ruff = "ruff"
+-- vim.g.lazyvim_python_lsp = "pyright"
+-- vim.g.lazyvim_python_ruff = "ruff"
 
 -- views can only be fully collapsed with the global statusline
 vim.opt.laststatus = 3
 
-vim.g.python3_host_prog = "/opt/miniconda3/bin/python3"
+vim.g.python3_host_prog = "/usr/bin/python3"
 
 vim.g.lazyvim_picker = "fzf"
