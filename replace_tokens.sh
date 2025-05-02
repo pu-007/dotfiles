e47e2a2c1e704453fd42911453c762d5e22c7686
Zion Pu <pu.007@qq.com>
2025-05-02 17:49:51 +0800

    refactor(script): Improve file path handling
    Use git rev-parse --show-toplevel to construct absolute path to config file.

--- replace_tokens.sh
+++ replace_tokens.sh
@@ -8,17 +8,9 @@ replace_token() {
   # 使用 sed 替换隐私 token，这里假设隐私 token 匹配正则表达式
   sed -E 's/(api_key|token|secret)=.*$/\1=REDACTED/gI' "$file" >"$temp_file" && mv "$temp_file" "$file"
   sed -E 's/(api_key|token|secret)\:.*$/\1\: REDACTED/gI' "$file" >"$temp_file" && mv "$temp_file" "$file"
-
-  # 检查是否进行了替换
-  if git diff --quiet "$file"; then
-    echo "No tokens found in $file"
-  else
-    echo "Token replaced in $file"
-    git add "$file" # 将修改后的文件添加到暂存区
-  fi
 }
 
-replace_token ./wsl.winuser/.config/yasb/config.yaml
+replace_token "$(git rev-parse --show-toplevel | tr -d '\n')"/wsl.winuser/.config/yasb/config.yaml
 
 # # 遍历所有传入的文件
 # for file in "$@"; do
