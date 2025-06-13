0564c9347323fb32b3c829d142d552298a1f63af
Zion Pu <pu.007@qq.com>
2025-06-13 21:39:18 +0800

    feat: Configure Conda and update autostart scripts
    feat: Add Conda initialization to PowerShell profile.
    
    feat: Create and configure .condarc for Windows with Tsinghua mirrors.
    
    fix: Update autostart VBS scripts to use 'python' directly.
    
    fix: Enable SSL verification in python.user/.condarc.

--- c.mnt/Program Files/PowerShell/7/profile.ps1
+++ c.mnt/Program Files/PowerShell/7/profile.ps1
@@ -62,3 +62,11 @@ function v {
 function e {
     explorer .
 }
+
+
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+If (Test-Path "C:\ProgramData\miniconda3\Scripts\conda.exe") {
+    (& "C:\ProgramData\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+}
+#endregion
