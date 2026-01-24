fe59a6a9246052839fbcee531d3e498ec73389a6
Zion Pu <pu.007@qq.com>
2026-01-24 19:15:49 +0800

    refactor: remove Neovim configuration files
    This commit removes the entire Neovim configuration directory and its contents.
    
    This includes:
    - Initialization files (init.lua)
    - Plugin configurations (lazy.lua, general.lua, linux.lua, windows.lua)
    - Keymap and autocmd configurations (keymaps.lua, autocmds.lua)
    - Options and theme settings (options.lua, colorscheme)
    - Build and formatting configurations (stylua.toml)
    - Unused files like LICENSE, README.md, and ginit.bak.vim
    - LazyVim specific configurations (lazyvim.json)
    
    The Neovim configuration was previously symlinked from `/home/pu/.config/nvim`.

--- nvim.user/.config/nvim/ginit.bak.vim
+++
@@ -1,3 +0,0 @@
-GuiFont! JetXW:h13
-GuiWindowOpacity 0.9
-source $VIMRUNTIME/mswin.vim
--- nvim.user/.config/nvim/lua/plugins/windows.lua
+++
@@ -1,7 +0,0 @@
-if vim.loop.os_uname().sysname == "Windows_NT" then
-  return {
-    "equalsraf/neovim-gui-shim",
-  }
-else
-  return {}
-end
